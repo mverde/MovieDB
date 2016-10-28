d98f5a4fef60097f53e6bd7031cd6d72ebc5b7a8
Martin Verde <martinthespartan23@gmail.com>
2016-10-28 15:42:26 -0700

    Added navbar template.

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <!DOCTYPE html>
 <html>
+	<head>
+		<title>MovieDB</title>
+
+		<!-- Bootstrap core CSS -->
+    	<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	</head>
 	<body>
-		
+		<nav class="navbar navbar-inverse navbar-static-top">
+      		<div class="container-fluid">
+        		<div class="navbar-header">
+          			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            			<span class="sr-only">Toggle navigation</span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+          			</button>
+          			<a class="navbar-brand" href="#">MovieDB</a>
+        		</div>
+
+        		<!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse">
+			      <ul class="nav navbar-nav">
+			        <li class="active"><a href="#">Search <span class="sr-only">(current)</span></a></li>
+			        <li><a href="#">Add</a></li>
+			        <li><a href="#">Actor A-Z</a></li>
+			        <li><a href="#">Movie A-Z</a></li>
+			      </ul>
+			  </div>
+     	 	</div>
+		</nav>
 	</body>
 </html>
