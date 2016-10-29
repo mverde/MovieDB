6a0a37d8f6ee4489d31c378614fbc2dab1c1e2bb
brmi <desireelenart@gmail.com>
2016-10-29 16:21:42 -0700

    Hooked up nav links

--- addcomment.php
+++ addcomment.php
@@ -24,7 +24,7 @@
 			    <div class="collapse navbar-collapse">
 			      <ul class="nav navbar-nav">
 			        <li class="active"><a href="index.php">Search <span class="sr-only">(current)</span></a></li>
-			        <li><a href="#">Add</a></li>
+			        <li><a href="add.php">Add</a></li>
 			      </ul>
 			  </div>
      	 	</div>
--- movieinfo.php
+++ movieinfo.php
@@ -9,28 +9,26 @@
     </head>
     <body>
         <nav class="navbar navbar-inverse navbar-static-top">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">MovieDB</a>
-                </div>
+      		<div class="container-fluid">
+        		<div class="navbar-header">
+          			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            			<span class="sr-only">Toggle navigation</span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+            			<span class="icon-bar"></span>
+          			</button>
+          			<a class="navbar-brand" href="#">MovieDB</a>
+        		</div>
 
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse">
-                  <ul class="nav navbar-nav">
-                    <li class="active"><a href="#">Search <span class="sr-only">(current)</span></a></li>
-                    <li><a href="#">Add</a></li>
-                    <li><a href="browseactor.php">Actor A-Z</a></li>
-                    <li><a href="browsemovie.php">Movie A-Z</a></li>
-                  </ul>
-              </div>
-            </div>
-        </nav>
+        		<!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse">
+			      <ul class="nav navbar-nav">
+			        <li class="active"><a href="index.php">Search <span class="sr-only">(current)</span></a></li>
+			        <li><a href="add.php">Add</a></li>
+			      </ul>
+			  </div>
+     	 	</div>
+		</nav>
         <!-- Connect to db -->
         <?php
 	        $mysqli = new mysqli("localhost", "cs143", "", "CS143", 1438);
